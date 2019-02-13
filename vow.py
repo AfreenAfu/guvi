205d61f5e1ad58160e29a73b2908332e0878bdea
AfreenAfu <42183362+AfreenAfu@users.noreply.github.com>
2019-02-13 14:29:13 +0530

    Update vow.py

--- vow.py
+++ vow.py
@@ -1,7 +1,7 @@
 letter = input()
 if letter in ('a', 'e', 'i', 'o', 'u'):
   print("Vowel")
-  elif letter != ('a', 'e', 'i', 'o', 'u'):
+  elif letter == ('b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z'):
     print("Consonant")
     else:
     print("Invalid")
